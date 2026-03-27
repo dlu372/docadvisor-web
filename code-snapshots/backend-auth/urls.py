3510dd510058beb02f9a94bc5e580afc8f5f8ab4
Di Lu <160996449+dlu372@users.noreply.github.com>
2026-03-27 23:00:19 +1300

    Update urls.py

--- code-snapshots/backend-auth/urls.py
+++ code-snapshots/backend-auth/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import login_user, register, reset
 
+urlpatterns = [
+    path('login', login_user),
+    path('retrieve', reset),
+    path('register', register),
+]
