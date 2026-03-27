ff9a4ec09b9ea18c77c84d222bb10221a569f78a
Di Lu <160996449+dlu372@users.noreply.github.com>
2026-03-27 22:55:48 +1300

    Update UserRunnables.java

--- code-snapshots/android-auth/UserRunnables.java
+++ code-snapshots/android-auth/UserRunnables.java
@@ -1,1 +1,43 @@
+package com.example.medicine_integration.net.data_base;
 
+public class UserRunnables extends BaseRunnables {
+
+    public Runnable LoginSuccess;
+    public Runnable WrongPassword;
+    public Runnable NoAccount;
+    public Runnable RegisterSuccess;
+    public Runnable AccountAlreadyHave;
+    public Runnable ResetSuccess;
+
+    public void setLoginSuccess(Runnable loginSuccess) {
+        LoginSuccess = loginSuccess;
+    }
+
+    public void setWrongPassword(Runnable wrongPassword) {
+        WrongPassword = wrongPassword;
+    }
+
+    public void setNoAccount(Runnable noAccount) {
+        NoAccount = noAccount;
+    }
+
+    public void setRegisterSuccess(Runnable registerSuccess) {
+        RegisterSuccess = registerSuccess;
+    }
+
+    public void setAccountAlreadyHave(Runnable accountAlreadyHave) {
+        AccountAlreadyHave = accountAlreadyHave;
+    }
+
+    public void setResetSuccess(Runnable resetSuccess) {
+        ResetSuccess = resetSuccess;
+    }
+
+    public void setConnectException(Runnable connectException) {
+        ConnectException = connectException;
+    }
+
+    public void setSocketTimeOut(Runnable socketTimeOut) {
+        SocketTimeOut = socketTimeOut;
+    }
+}
